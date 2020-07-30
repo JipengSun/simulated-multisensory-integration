d42ef159b870e464561f48913399fc8a1f615369
Lukas <lukasbill@163.com>
2020-07-30 21:17:28 +0800

    Learn PyTorch

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+'''
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import animation
@@ -14,3 +15,37 @@ def animate(i):
 
 anim = animation.FuncAnimation(fig, animate, frames=200, interval=60, blit=True)
 plt.show()
+'''
+
+from __future__ import print_function
+import torch
+'''
+x = torch.empty(5,3)
+print (x)
+y = torch.zeros(5,3,dtype=torch.long)
+print (x[:,1])
+z = x.view(-1,3)
+a = z.numpy()
+z.add_(1)
+print (z)
+print (a)
+'''
+
+x = torch.ones(2,2,requires_grad=True)
+print (x)
+y = x + 2
+print (y)
+print (y.grad_fn)
+z = y * y * 3
+out = z.mean()
+print (z, out)
+
+a = torch.randn(2,2)
+a = ((a*3)/(a-1))
+print (a.requires_grad)
+a.requires_grad_(True)
+print (a.requires_grad)
+b = (a*a).sum()
+print (b.grad_fn)
+out.backward()
+print(x.grad)
