80a69db43a0451140e421486690e12f31648adec
Lukas <lukasbill@163.com>
2020-08-13 14:52:53 +0800

    Improve the visual inference performance by using masked target only.

--- multisensory_integration/sensory_gathering.py
+++ multisensory_integration/sensory_gathering.py
@@ -1,6 +1,7 @@
 import sys
 import cv2
 import os
+import numpy as np
 
 video_src_path = '/Users/Jipeng/PycharmProjects/simulated_multisensory_integration/data/video/'
 image_src_path = '/Users/Jipeng/PycharmProjects/simulated_multisensory_integration/data/images/'
@@ -29,17 +30,56 @@ def frame_writer(videopath,imagepath):
 # Convert the video to images. Frames in the same video will be saved into same folder.
 def video2images(idx):
     video_path = video_src_path + '2-dof_video_%s.mp4' % idx
-    image_folder_path = image_src_path + str(idx)
+    image_folder_path = image_src_path +'original/' +str(idx)
     if not os.path.exists(image_folder_path):
         os.mkdir(image_folder_path)
     frame_writer(video_path,image_folder_path + '/')
-if __name__ == '__main__':
-    # Clear all the files and directories under the root folder.
-    for root, dirs, files in os.walk(image_src_path, topdown=False):
+
+ # Clear all the files and directories under the root folder.
+def delete_cascade(root_path):
+    for root, dirs, files in os.walk(root_path, topdown=False):
         for name in files:
             os.remove(os.path.join(root, name))
         for name in dirs:
             os.rmdir(os.path.join(root, name))
+def mask_frame_writer(videopath,imagepath):
+    camera = cv2.VideoCapture(videopath)
+    #width = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))
+    #height = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    index = 0
+    if not camera.isOpened():
+        print ('Could not open video')
+        sys.exit()
+    while True:
+        res, frame = camera.read()
+        if not res:
+            break
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        # print hsv
+        l_blue = np.array([100, 43, 46])
+        h_blue = np.array([124, 255, 255])
+        mask = cv2.inRange(hsv, l_blue, h_blue)
+        #res = cv2.bitwise_and(frame, frame, mask=mask)
+        image_name = 'mask%s.jpg' % index
+        cv2.imwrite(imagepath + image_name, mask)
+        index += 1
+
+def video2mask(idx):
+    video_path = video_src_path + '2-dof_video_%s.mp4' % idx
+    image_folder_path = image_src_path +'mask/'+ str(idx)
+    if not os.path.exists(image_folder_path):
+        os.mkdir(image_folder_path)
+    mask_frame_writer(video_path,image_folder_path + '/')
+if __name__ == '__main__':
+    '''
+    # Convert original frames
+    delete_cascade(image_src_path+'original/')
     for i in range(50):
         video2images(i)
         print ('Video %s is converted'%i)
+    '''
+    # Convert mask frames
+    delete_cascade(image_src_path+'mask/')
+    for i in range(50):
+        video2mask(i)
+        print ('Video %s is converted to masked images' % i)
--- test.py
+++ test.py
@@ -30,7 +30,7 @@ z.add_(1)
 print (z)
 print (a)
 '''
-
+'''
 x = torch.ones(2,2,requires_grad=True)
 print (x)
 y = x + 2
@@ -49,3 +49,35 @@ b = (a*a).sum()
 print (b.grad_fn)
 out.backward()
 print(x.grad)
+'''
+import sys
+import cv2
+import os
+import numpy as np
+
+video_src_path = '/Users/Jipeng/PycharmProjects/simulated_multisensory_integration/data/video/'
+image_src_path = '/Users/Jipeng/PycharmProjects/simulated_multisensory_integration/data/images/'
+file_name = '2-dof_video.mp4'
+def frame_writer(videopath,imagepath):
+    camera = cv2.VideoCapture(videopath)
+    #width = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))
+    #height = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    index = 0
+    if not camera.isOpened():
+        print ('Could not open video')
+        sys.exit()
+    while True:
+        res, frame = camera.read()
+        if not res:
+            break
+        hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+        #print hsv
+        l_blue = np.array([100,43,46])
+        h_blue = np.array([124,255,255])
+        mask = cv2.inRange(hsv,l_blue,h_blue)
+        res = cv2.bitwise_and(frame,frame,mask= mask)
+        cv2.imshow('frame',frame)
+        cv2.imshow('mask',mask)
+        cv2.imshow('res',res)
+        cv2.waitKey(0)
+frame_writer(video_src_path+'2-dof_video_0.mp4',None)
